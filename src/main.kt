871fbbf3546e04c0752bf8fb1b1135410b3bab90
kazuya-seki <track613armory@gmail.com>
2018-06-30 22:26:47 +0900

    ファイル読み込み方を学ぶ

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    // ファイルを読み込む
+    fun readFileDirectlyAsText(filepath: String): String
+            = File(filepath).readText(Charsets.UTF_8)
+    val filepath = "./sample/script.st"
+    println(readFileDirectlyAsText(filepath))
 }
